e73a5f56920d4cb5f5626f0ccb022881e571fdb6
Apoorv Garg <u.garg.10@gmail.com>
2020-03-17 10:23:20 +0530

    GridReady

---
+++ Assets/Testing.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class Testing : MonoBehaviour
+{
+    Grid grid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = new Grid(4, 2, 10f, new Vector3(20,0));
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(),1);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+        }
+
+    }
+}
