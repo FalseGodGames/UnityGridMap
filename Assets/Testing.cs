13e31d96ee6ce0ce6002d6cef666a5e653af4eeb
Apoorv Garg <u.garg.10@gmail.com>
2020-03-18 11:41:31 +0530

    HeatMapVisual
    Not Complete

--- Assets/Testing.cs
+++ Assets/Testing.cs
@@ -9,22 +9,8 @@ public class Testing : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        grid = new Grid(4, 2, 10f, new Vector3(20,0));
+        grid = new Grid(20, 10, 10f, Vector3.zero);
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            grid.SetValue(UtilsClass.GetMouseWorldPosition(),1);
-        }
-
-        if (Input.GetMouseButtonDown(1))
-        {
-            Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
-        }
-
-    }
 }
